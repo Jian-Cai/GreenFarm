9b0431ba95ff3adde349d250d3c7c3ac8bb93693
DuoYao <ytydhgx@icloud.com>
2019-09-01 14:12:48 +0800

    update on 9.1

--- src/main/java/org/csu/greenfarm/controller/AccountController.java
+++ src/main/java/org/csu/greenfarm/controller/AccountController.java
@@ -15,28 +15,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/*
+跳转至登录界面&个人中心界面
+ */
 @Controller
 public class AccountController {
 
     @Autowired
     AccountService service;
 
-    @GetMapping("/")
-    public String viewLogin() {
-        return "index";
+    @Autowired
+    HttpServletRequest request;
+
+    //跳转至个人中心界面
+    @GetMapping("/account/account")
+    public String showaccount(){
+        HttpSession session = request.getSession();
+        //如果session中的account为空则去登录
+        return session.getAttribute("account") == null ? "login/login" : "account/account";
     }
 
-
+    //跳转至登录界面
     @PostMapping("/tologin")
     public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
+        //通过邮箱、账户、昵称登录
         Account account = service.getAccountByAccount(username) == null ? (service.getAccountByMail(username) == null ? service.getAccountByUsername(username) : service.getAccountByMail(username)) : service.getAccountByAccount(username);
-        if (account.getPassword().equals(password))
-            return "index";
-        else return "account/login";
+        if (account.getPassword().equals(password)){
+            HttpSession session = request.getSession();
+            session.setAttribute("account", account);
+            return "account/account";
+        }
+        else{
+
+            return "login/login";
+        }
     }
 
     @GetMapping("/toregister")
     public String toRegister(){
-        return "account/register";
+        return "login/register";
     }
 }
--- src/main/java/org/csu/greenfarm/controller/FarmController.java
+++ src/main/java/org/csu/greenfarm/controller/FarmController.java
@@ -1,4 +1,32 @@
+/*
+ * Copyright (c) 2019
+ * User: Duo.y
+ * File: FarmController.java
+ * Date: 2019/09/01 11:17:01
+ */
+
 package org.csu.greenfarm.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+/*
+农场页面以及农场业务逻辑展示跳转
+ */
+
+@Controller
 public class FarmController {
+
+    //return farmList
+    @GetMapping("/farm/farmList")
+    public String showFarmList(){
+        return "farm/farmList";
+    }
+
+    //return farm item by farmId
+    @GetMapping("/farm/farmItem")
+    public String showFarmItem(@RequestParam("farmId") String farmId){
+        return "farm/farmItem";
+    }
+
 }
