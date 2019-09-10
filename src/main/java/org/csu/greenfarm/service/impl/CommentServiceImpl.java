c50f6583cb55cf5e16fea7fae3c2404bce7ab3e0
DuoYao <ytydhgx@icloud.com>
2019-09-10 10:21:56 +0800

    新增农产品评论功能，优化评论显示

--- src/main/java/org/csu/greenfarm/service/impl/CommentServiceImpl.java
+++ src/main/java/org/csu/greenfarm/service/impl/CommentServiceImpl.java
@@ -1,7 +1,10 @@
 package org.csu.greenfarm.service.impl;
 
 import org.csu.greenfarm.domain.FarmComment;
+import org.csu.greenfarm.domain.ProductCom;
 import org.csu.greenfarm.persistence.FarmComMapper;
+import org.csu.greenfarm.persistence.ProductComMapper;
+import org.csu.greenfarm.persistence.ProductMapper;
 import org.csu.greenfarm.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,13 +18,22 @@ public class CommentServiceImpl implements CommentService {
     @Autowired
     private FarmComMapper farmComMapper;
 
+    @Autowired
+    private ProductComMapper productComMapper;
+
     @Override
     public List<Object> getCommentByItemId(String itemId) {
         List<Object> o = new ArrayList<>();
         List<FarmComment> farmComments = farmComMapper.getFarmCommentByFarmId(itemId);
-        if(farmComments != null){
+        List<ProductCom> productComs = productComMapper.getProductCommentByProductId(itemId);
+        if(farmComments.size() != 0){
             for(int a = 0; a < farmComments.size(); a++){
                 o.add(farmComments.get(a));
+            }
+        }
+        else {
+            for(int a = 0; a < productComs.size(); a++){
+                o.add(productComs.get(a));
             }
         }
         return o;
@@ -31,9 +43,15 @@ public class CommentServiceImpl implements CommentService {
     public List<Object> getCommentByUsername(String username) {
         List<Object> o = new ArrayList<>();
         List<FarmComment> farmComments = farmComMapper.getFarmCommentByUsername(username);
-        if(farmComments != null){
+        List<ProductCom> productComs = productComMapper.getProductCommentByUsername(username);
+        if(farmComments.size() != 0){
             for(int a = 0; a < farmComments.size(); a++){
                 o.add(farmComments.get(a));
+            }
+        }
+        if(productComs.size() != 0){
+            for(int a = 0; a < productComs.size(); a++){
+                o.add(productComs.get(a));
             }
         }
         return o;
@@ -43,6 +61,9 @@ public class CommentServiceImpl implements CommentService {
     public void insertComment(Object o) {
         if(o.getClass().getName().equals("org.csu.greenfarm.domain.FarmComment")){
             farmComMapper.insertFarmComment((FarmComment) o);
+        }
+        if(o.getClass().getName().equals("org.csu.greenfarm.domain.ProductCom")){
+            productComMapper.insertProductComment((ProductCom) o);
         }
     }
 }
